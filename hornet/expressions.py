24e13d89a556c7e10c785ac00253b0cccb1ad9b0
pillmuncher <m.krippendorf@freenet.de>
2014-09-27 02:23:35 +0200

    Version 0.2.3a

--- hornet/expressions.py
+++ hornet/expressions.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2014 Mick Krippendorf <m.krippendorf@freenet.de>
 
 __version__ = '0.2.3a'
-__date__ = '2014-08-12'
+__date__ = '2014-09-27'
 __author__ = 'Mick Krippendorf <m.krippendorf@freenet.de>'
 __license__ = 'MIT'
 
