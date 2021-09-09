6bf6ce541dd5a1e97400104afbcd5814573af3c5
Mick Krippendorf <m.krippendorf@freenet.de>
2021-09-10 01:10:40 +0200

    removed mock trampoline code.

--- src/hornet/tailcalls.py
+++ src/hornet/tailcalls.py
@@ -9,40 +9,22 @@ __author__ = 'Mick Krippendorf <m.krippendorf@freenet.de>'
 __license__ = 'MIT'
 
 
-USE_TCO = True
+from functools import wraps
 
-if USE_TCO:
 
-    import functools
+def tco(function):
+    @wraps(function)
+    def launch(*args, **kwargs):
+        return function, (), args, kwargs
+    return launch
 
-    def trampoline(bouncing, *args, **kwargs):
-        while bouncing:
-            bouncing, result, args, kwargs = bouncing(*args, **kwargs)
-            yield from result
+def abort(*args, **kwargs):
+    return None, (), args, kwargs
 
-    def abort(*args, **kwargs):
-        return None, (), args, kwargs
+def emit(cont, *args, _=None, **kwargs):
+    return cont, [_], args, kwargs
 
-    def emit(cont, *args, _=None, **kwargs):
-        return cont, [_], args, kwargs
-
-    def bounce(cont, *args, **kwargs):
-        return cont, (), args, kwargs
-
-    def tco(function):
-        return functools.partial(bounce, function)
-
-else:
-
-    from toolz.functoolz import identity as tco
-
-    def trampoline(f, *a, **k):
-        return f(*a, **k)
-
-    def abort():
-        return
-        yield
-
-    def emit(maybe_more_solutions):
-        yield
-        yield from maybe_more_solutions()
+def trampoline(bounce, *args, **kwargs):
+    while bounce:
+        bounce, result, args, kwargs = bounce(*args, **kwargs)
+        yield from result
