51f16076c2ac0267a688f1d763e52e189de73361
Mick Krippendorf <m.krippendorf@freenet.de>
2023-06-28 17:59:34 +0200

    cleaned up test_dcg.py

--- src/test/test_dcg.py
+++ src/test/test_dcg.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 Mick Krippendorf <m.krippendorf@freenet.de>
+# Copyright (z) 2014 Mick Krippendorf <m.krippendorf@freenet.de>
 
 __version__ = '0.2.5a'
 __date__ = '2014-09-27'
@@ -18,8 +18,7 @@ def test_dcg_transformation():
     from hornet.dcg import expand, _C_
 
     from hornet.symbols import s, np, vp, _0, _1, _2, _3, _4, _5, _6, _7, _8
-    from hornet.symbols import it_is, tell, me, it, aint, so, very, different
-    from hornet.symbols import a, b, c, X
+    from hornet.symbols import a, b, c, d, e, f, g, h, x, y, z, X
 
     expression_all(
         ast_eq,
@@ -34,144 +33,154 @@ def test_dcg_transformation():
             s(_0, _2) << np(_0, _1) & vp(_1, _2),
         ),
         (
-            s >> np(a) & vp(a, b),
-            s(_0, _2) << np(a, _0, _1) & vp(a, b, _1, _2),
+            s >> np(x) & vp(x, y),
+            s(_0, _2) << np(x, _0, _1) & vp(x, y, _1, _2),
         ),
         (
-            s >> a & b & c,
-            s(_0, _3) << a(_0, _1) & b(_1, _2) & c(_2, _3),
+            s >> x & y & z,
+            s(_0, _3) << x(_0, _1) & y(_1, _2) & z(_2, _3),
         ),
         (
-            it_is(X) >> [] & [],
-            it_is(X, _0, _0),
+            a(X) >> [] & [],
+            a(X, _0, _0),
         ),
         (
-            it_is(X) >> [],
-            it_is(X, _0, _0),
+            a(X) >> [],
+            a(X, _0, _0),
         ),
         (
-            it_is(X) >> [] & so,
-            it_is(X, _0, _1) <<
-            so(_0, _1),
+            a(X) >> 
+                [] & f,
+            a(X, _0, _1) <<
+            f(_0, _1),
         ),
         (
-            it_is(X) >> so & [],
-            it_is(X, _0, _1) <<
-            so(_0, _1),
+            a(X) >> 
+                f & [],
+            a(X, _0, _1) <<
+                f(_0, _1),
         ),
         (
-            it_is(X) >> [] & so & [],
-            it_is(X, _0, _1) <<
-            so(_0, _1),
+            a(X) >> 
+                [] & f & [],
+            a(X, _0, _1) <<
+                f(_0, _1),
         ),
         (
-            it_is(X) >> tell & [] & so,
-            it_is(X, _0, _2) <<
-            tell(_0, _1) &
-            so(_1, _2),
+            a(X) >> 
+                b & [] & f,
+            a(X, _0, _2) <<
+                b(_0, _1) &
+                f(_1, _2),
         ),
         (
-            it_is(X) >> [tell],
-            it_is(X, _0, _1) <<
-            _C_(_0, tell, _1),
+            a(X) >> 
+                [b],
+            a(X, _0, _1) <<
+                _C_(_0, b, _1),
         ),
         (
-            it_is(X) >> [tell, me],
-            it_is(X, _0, _2) <<
-            _C_(_0, tell, _1) &
-            _C_(_1, me, _2),
+            a(X) >> 
+                [b, c],
+            a(X, _0, _2) <<
+                _C_(_0, b, _1) &
+                _C_(_1, c, _2),
         ),
         (
-            it_is(X) >> [tell, me, it],
-            it_is(X, _0, _3) <<
-            _C_(_0, tell, _1) &
-            _C_(_1, me, _2) &
-            _C_(_2, it, _3),
+            a(X) >> 
+                [b, c, d],
+            a(X, _0, _3) <<
+                _C_(_0, b, _1) &
+                _C_(_1, c, _2) &
+                _C_(_2, d, _3),
         ),
         (
-            it_is(X) >> [tell, me, it, aint],
-            it_is(X, _0, _4) <<
-            _C_(_0, tell, _1) &
-            _C_(_1, me, _2) &
-            _C_(_2, it, _3) &
-            _C_(_3, aint, _4),
+            a(X) >> 
+                [b, c, d, e],
+            a(X, _0, _4) <<
+                _C_(_0, b, _1) &
+                _C_(_1, c, _2) &
+                _C_(_2, d, _3) &
+                _C_(_3, e, _4),
         ),
         (
-            it_is(X) >> [tell, me, it, aint, so],
-            it_is(X, _0, _5) <<
-            _C_(_0, tell, _1) &
-            _C_(_1, me, _2) &
-            _C_(_2, it, _3) &
-            _C_(_3, aint, _4) &
-            _C_(_4, so, _5),
+            a(X) >>
+                [b, c, d, e, f],
+            a(X, _0, _5) <<
+                _C_(_0, b, _1) &
+                _C_(_1, c, _2) &
+                _C_(_2, d, _3) &
+                _C_(_3, e, _4) &
+                _C_(_4, f, _5),
         ),
         (
-            (it_is(X) & [c, tell, me, it]) >>
-            [aint, so, a, b],
-            it_is(X, _0, _8) <<
-            _C_(_0, aint, _1) &
-            _C_(_1, so, _2) &
-            _C_(_2, a, _3) &
-            _C_(_3, b, _4) &
-            _C_(_8, c, _7) &
-            _C_(_7, tell, _6) &
-            _C_(_6, me, _5) &
-            _C_(_5, it, _4),
+            (a(X) & [z, b, c, d]) >>
+                [e, f, x, y],
+            a(X, _0, _8) <<
+                _C_(_0, e, _1) &
+                _C_(_1, f, _2) &
+                _C_(_2, x, _3) &
+                _C_(_3, y, _4) &
+                _C_(_8, z, _7) &
+                _C_(_7, b, _6) &
+                _C_(_6, c, _5) &
+                _C_(_5, d, _4),
         ),
         (
-            (it_is(X) & [tell, me]) >>
-            [very, different],
-            it_is(X, _0, _4) <<
-            _C_(_0, very, _1) &
-            _C_(_1, different, _2) &
-            _C_(_4, tell, _3) &
-            _C_(_3, me, _2),
+            (a(X) & [b, c]) >>
+                [g, h],
+            a(X, _0, _4) <<
+                _C_(_0, g, _1) &
+                _C_(_1, h, _2) &
+                _C_(_4, b, _3) &
+                _C_(_3, c, _2),
         ),
         (
-            it_is(X) & [me] >>
-            it & [very] & so,
-            it_is(X, _0, _4) <<
-            it(_0, _1) &
-            _C_(_1, very, _2) &
-            so(_2, _3) &
-            _C_(_4, me, _3),
+            a(X) & [c] >>
+                d & [g] & f,
+            a(X, _0, _4) <<
+                d(_0, _1) &
+                _C_(_1, g, _2) &
+                f(_2, _3) &
+                _C_(_4, c, _3),
         ),
         (
-            it_is(X) & [me] >>
-            it & [very] & so & [different],
-            it_is(X, _0, _5) <<
-            it(_0, _1) &
-            _C_(_1, very, _2) &
-            so(_2, _3) &
-            _C_(_3, different, _4) &
-            _C_(_5, me, _4),
+            a(X) & [c] >>
+                d & [g] & f & [h],
+            a(X, _0, _5) <<
+                d(_0, _1) &
+                _C_(_1, g, _2) &
+                f(_2, _3) &
+                _C_(_3, h, _4) &
+                _C_(_5, c, _4),
+            ),
+        (
+            a(X) & [c] >>
+                promote([d]) & [g] & [b] & [h] & f,
+            a(X, _0, _6) <<
+                _C_(_0, d, _1) &
+                _C_(_1, g, _2) &
+                _C_(_2, b, _3) &
+                _C_(_3, h, _4) &
+                f(_4, _5) &
+                _C_(_6, c, _5),
         ),
         (
-            it_is(X) & [me] >>
-            promote([it]) & [very] & [tell] & [different] & so,
-            it_is(X, _0, _6) <<
-            _C_(_0, it, _1) &
-            _C_(_1, very, _2) &
-            _C_(_2, tell, _3) &
-            _C_(_3, different, _4) &
-            so(_4, _5) &
-            _C_(_6, me, _5),
+            a(X) & [c] >>
+                d & [g, b] & f & [h],
+            a(X, _0, _6) <<
+                d(_0, _1) &
+                _C_(_1, g, _2) &
+                _C_(_2, b, _3) &
+                f(_3, _4) &
+                _C_(_4, h, _5) &
+                _C_(_6, c, _5),
         ),
         (
-            it_is(X) & [me] >>
-            it & [very, tell] & so & [different],
-            it_is(X, _0, _6) <<
-            it(_0, _1) &
-            _C_(_1, very, _2) &
-            _C_(_2, tell, _3) &
-            so(_3, _4) &
-            _C_(_4, different, _5) &
-            _C_(_6, me, _5),
-        ),
-        (
-            it_is(X) >> {tell(X)} & me,
-            it_is(X, _0, _1) <<
-            tell(X) &
-            me(_0, _1),
+            a(X) >> 
+                {b(X)} & c,
+            a(X, _0, _1) <<
+                b(X) &
+                c(_0, _1),
         ),
     )
